f586cac0fe89a0754b9b6684081fc9256d4b46da
Amilton Machado <amilton.machado@fw7.com.br>
2022-09-12 12:10:25 -0300

    feat(User): adjusts on User

--- src/main/java/br/com/amxsistemas/phoenix/PhoenixApplication.java
+++ src/main/java/br/com/amxsistemas/phoenix/PhoenixApplication.java
@@ -13,7 +13,7 @@ public class PhoenixApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PhoenixApplication.class, args);
 		Language.setLanguage("PT-BR");
-		UserLogger.setUserLog(UUID.fromString("6c90e522-bbf3-4964-98c8-f7c7c2569e3a"));
+		UserLogger.setUserLog(UUID.fromString("6caef3e5-a173-4e5f-97aa-da56a413ee0a"));
 	}
 
 }
