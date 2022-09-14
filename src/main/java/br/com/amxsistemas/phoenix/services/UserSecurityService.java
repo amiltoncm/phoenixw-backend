381e51eec05f6611a18a4e475d07b02b995403f0
Lucas <lucasoliveira_aky@outlook.com.br>
2022-09-13 21:15:55 -0300

    feat: Implements UserDetails builder

--- src/main/java/br/com/amxsistemas/phoenix/services/UserSecurityService.java
+++ src/main/java/br/com/amxsistemas/phoenix/services/UserSecurityService.java
@@ -2,6 +2,7 @@ package br.com.amxsistemas.phoenix.services;
 
 import br.com.amxsistemas.phoenix.repositories.UserRepository;
 import br.com.amxsistemas.phoenix.utils.messages.Messages;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,14 +11,18 @@ public class UserSecurityService implements UserDetailsService {
 
     final UserRepository userRepository;
 
-    public UserSecurityService (UserRepository userRepository) {
+    public UserSecurityService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         var opt = userRepository.findByName(username);
-        var user = opt.orElseThrow(()-> new UsernameNotFoundException(Messages.getMessage(5)));
-        return new UserDetails(user.getUserName(), user.getPassword());
+        var user = opt.orElseThrow(() -> new UsernameNotFoundException(Messages.getMessage(5)));
+
+        User.UserBuilder builder = User.withUsername(user.getName());
+        builder.password(user.getPassword());
+
+        return builder.build();
     }
 }
